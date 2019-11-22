1a368ee58ec100a1d16a15e72823738e32a1bac2
nishadotcom <nisha.com126@gmail.com>
2019-11-23 01:35:30 +0530

    Adding Google Key

--- Source/application/config/google_config.php
+++ Source/application/config/google_config.php
@@ -1,6 +1,6 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
-$config['google_client_id']="480061210784-vtikld7btq04f8r9v9aecjtkp00juq66.apps.googleusercontent.com";
-$config['google_client_secret']="zO16KGEckQn828lIO6FzFCXp";
+$config['google_client_id']="1069948152458-gukpn130hfri5aj5ebrapcjim726qb1k.apps.googleusercontent.com";
+$config['google_client_secret']="QNBLgc3pVyfhJ3xTk7Jakc4Q";
 $config['google_redirect_url']=base_url().'auth/oauth2callback';
 
